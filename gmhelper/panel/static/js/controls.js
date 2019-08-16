ef1ed7da35273801af373d91934ef213c8d9c136
aknecht2 <aknecht2@unl.edu>
2019-08-16 12:20:07 -0400

    Added more autocomplete functionality.

--- gmhelper/panel/static/js/controls.js
+++ gmhelper/panel/static/js/controls.js
@@ -1,6 +1,10 @@
 var socket = new WebSocket("ws://" + window.location.host + "/ws/session_control/");
 // Used for timing delay for auto complete.
 var debounce;
+// Used to keep track of current list of items in autocomplete.
+var auto_complete_items;
+// Currently selected item in list.
+var current_selection = -1;
 
 function send_message(e) {
   socket.send(JSON.stringify(e));
@@ -16,12 +20,12 @@ function auto_complete(keyword) {
     url: "/panel/auto_complete?keyword=" + keyword,
     dataType: "json"
   }).done(function(result) {
-    var data = jQuery.parseJSON(result);
+    auto_complete_items = jQuery.parseJSON(result);
     var html_string = "";
-    for (var i = 0; i < data.length; i++) {
-      var model_name = data[i].model.split(".")[1];
+    for (var i = 0; i < auto_complete_items.length; i++) {
+      var model_name = auto_complete_items[i].model.split(".")[1];
       model_name = model_name.charAt(0).toUpperCase() + model_name.slice(1);
-      html_string += "<li class='entry' data-id='" + data[i].pk + "' data-model='" + data[i].model + "'>" + data[i].fields.name + "<span class='item-type'>" + model_name + "</span></li>";
+      html_string += "<li class='entry' data-index='" + i + "'>" + auto_complete_items[i].fields.name + "<span class='item-type'>" + model_name + "</span></li>";
     }
     $("#auto-complete-results").html(html_string);
     $("#auto-complete-results").show();
@@ -36,6 +40,51 @@ $(function() {
     });
   });
 
+  $("#auto-complete-results").on("click", ".entry", function() {
+    var index = $(this).attr("data-index");
+    var data = auto_complete_items[index];
+    console.log(data);
+    $("#auto-complete-results").hide();
+  });
+
+  $("#auto-complete-results").on("mouseenter", ".entry", function() {
+    $(this).addClass("selected");
+  });
+
+  $("#auto-complete-results").on("mouseleave", ".entry", function() {
+    $(this).removeClass("selected");
+  });
+
+  $("#input-keyword").on("focus", function() {
+    if ($("#auto-complete-results").children().length > 0) {
+      $("#auto-complete-results").show();
+    }
+  });
+
+  $("#input-keyword").on("focusout", function() {
+    // Set timeout so that click event still fires... gross.
+    setTimeout(function() {
+      $("#auto-complete-results").hide();
+    }, 10);
+  });
+
+  $("#input-keyword").on("keydown", function(event) {
+    // Scroll the selected class
+    if (event.key == "ArrowDown" || event.key == "ArrowUp") {
+      if ($("#auto-complete-results").children().length == 0) return;
+      var direction = (event.key == "ArrowUp") ? -1 : 1;
+      if (current_selection > -1) {
+        $($("#auto-complete-results").children()[current_selection]).removeClass("selected");
+      }
+      current_selection = (current_selection + direction + 5) % 5;
+      $($("#auto-complete-results").children()[current_selection]).addClass("selected");
+    // Add the selected item.
+    } else if (event.key == "Enter") {
+      if (current_selection < 0) return;
+      $($("#auto-complete-results").children()[current_selection]).click();
+    }
+  });
+
   /**
    * Aww yiss, roll your own debounce.
    */
@@ -45,7 +94,7 @@ $(function() {
     if (keyword.length > 0) {
       debounce = setTimeout(function() {
         auto_complete(keyword);
-      }, 250);
+      }, 200);
     } else {
       $("#auto-complete-results").hide();
       $("#auto-complete-results").empty();
